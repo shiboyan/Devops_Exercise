d0f831439c1a104082f4cf2f15930bac9506d8ac
shiboyan <47836539+shiboyan@users.noreply.github.com>
2020-07-19 09:03:55 +1200

    Update print_aws_ip_ranges.sh
    Modify format

--- print_aws_ip_ranges.sh
+++ print_aws_ip_ranges.sh
@@ -3,22 +3,22 @@ function help(){
     cat <<- EOF
 	    Desc: the script use to print ip for specific region
 	    Usage: sh print_aws_ip_ranges.sh "region_name"
-		EOF
-		exit 0
+            EOF
+    exit 0
 }
 if [ ! -n "$1" ] ;then
-       help 
+    help 
 else
-        curl https://ip-ranges.amazonaws.com/ip-ranges.json > file
-        allregion=`cat file |grep  region |sort -u |awk '{print $NF}' |sed 's/\"//g' |sed 's/,//g'`
-	    var=$1
-        if [[ $allregion =~ $var ]]; then 
-		       cat file |grep -B 1 region |grep -B 1 $1 |grep "ip_prefix" |awk '{print $NF}' |sed s/,//g |sed s/\"//g
-               sum=$(cat file |grep -B 1 region |grep -B 1 $1 |grep "ip_prefix" |awk '{print $NF}' |sed s/,//g |sed s/\"//g |wc -l)
-               echo "total is "$sum
-		else
-              echo "erro invalid region"
-              exit -1
-		fi
+    curl https://ip-ranges.amazonaws.com/ip-ranges.json > file
+    allregion=`cat file |grep  region |sort -u |awk '{print $NF}' |sed 's/\"//g' |sed 's/,//g'`
+    var=$1
+    if [[ $allregion =~ $var ]]; then 
+        cat file |grep -B 1 region |grep -B 1 $1 |grep "ip_prefix" |awk '{print $NF}' |sed s/,//g |sed s/\"//g
+        sum=$(cat file |grep -B 1 region |grep -B 1 $1 |grep "ip_prefix" |awk '{print $NF}' |sed s/,//g |sed s/\"//g |wc -l)
+        echo "total is "$sum
+     else
+        echo "erro invalid region"
+        exit -1
+     fi
 fi
 
